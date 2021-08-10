e361520ae00006b5f4d5b016df95e356d81ccc54
danielnedefo <danilka4412@gmail.com>
2021-08-10 16:32:41 +0300

    added auth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
